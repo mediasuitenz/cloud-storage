3aeea47e3e225e24902593ce31c5df89fa6386eb
Richard Walker <digitalsadhu@gmail.com>
2016-01-06 11:41:37 +0100

    Remove unneeded comment, see readme instead

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict'
 
-// module usage looks something like:
-//
-// storage(config).upload('my-file.png', req.body, options).then(cb)
-//
-// storage(config).download('my-file.png', options).then(cb)
-
 const R = require('ramda')
 const adapter = require('./adapter')
 const fileCache = require('./file-cache')
