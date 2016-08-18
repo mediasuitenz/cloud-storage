db82f3e0628e2893fd3ddaffe7b60318f9acb542
Jonathan Prince <jonathan.prince@gmail.com>
2016-08-18 16:08:28 +0200

    extract image processing into separate file

--- index.js
+++ index.js
@@ -3,17 +3,7 @@
 const R = require('ramda')
 const adapter = require('./adapter')
 const fileCache = require('./file-cache')
-const stream = require('stream')
-const sharp = require('sharp')
-
-const renameThumb = (name, options) => {
-  const dotIndex = name.lastIndexOf('.')
-  const dotExtension = name.slice(dotIndex)
-  const filename = name.slice(0, dotIndex)
-  const label = options.label || `@${options.width}x${options.height}`
-
-  return `${filename}${label}${dotExtension}`
-}
+const processAndUploadImage = require('./image-processing')
 
 module.exports = config => {
   const defaults = {
@@ -39,48 +29,10 @@ module.exports = config => {
 
       if (!/^image\/.*/.test(options.ContentType)) {
         return client.upload(name, data, options).then(cache.put(name, data))
-      }
-
-      let originalData, fullsize
-
-      if ((data instanceof stream.Stream)) {
-        originalData = new stream.PassThrough()
-        data.pipe(originalData)
-        fullsize = originalData.pipe(sharp().resize(options.maxSize, options.maxSize).max().withoutEnlargement().on('info', info => options.meta = info))
       } else {
-        originalData = data
-        fullsize = sharp(originalData).resize(options.maxSize, options.maxSize).max().withoutEnlargement().on('info', info => options.meta = info)
+        const inputArgs = {name, data, options}
+        return processAndUploadImage(client, inputArgs, cache)
       }
-
-      const originalUpload = client.upload(name, fullsize, options).then(cache.put(name, data))
-      const uploads = [originalUpload]
-
-      // add check for mimetype before trying to do image processing
-
-      if (options.thumbnails && Array.isArray(options.thumbnails)) {
-        options.thumbnails.forEach(thumbOptions => {
-          const thumbname = renameThumb(name, thumbOptions)
-          const addMeta = meta => versionOptions.meta = meta
-          const versionOptions = R.clone(options)
-          versionOptions.isThumb = thumbOptions.isThumb
-          let resizedData
-
-          if (data instanceof stream.Stream) {
-            const dataCopy = new stream.PassThrough()
-            data.pipe(dataCopy)
-            resizedData = dataCopy.pipe(sharp().resize(thumbOptions.width, thumbOptions.height).on('info', addMeta))
-          } else {
-            resizedData = sharp(data).resize(thumbOptions.width, thumbOptions.height).on('info', addMeta)
-          }
-
-          const uploadPromise = client.upload(thumbname, resizedData, versionOptions).then(cache.put(thumbname, resizedData))
-          uploads.push(uploadPromise)
-        })
-      }
-
-      const returnValue = Promise.all(uploads)
-
-      return (returnValue.length === 1) ? returnValue[0] : returnValue
     },
     download (name, options) {
       options = options || {}
